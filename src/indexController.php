2a8e2f8b3d9164e0408978e2ff8a0b882415911c
Rinelsia <rinelsia@mail.ru>
2021-03-04 01:40:21 +0300

    Add files via upload

---
+++ src/indexController.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Controller;
+/**
+ * 
+ */
+class indexController 
+{
+	
+	function __construct()
+	{
+		# code...
+		echo "<p style ='color:#fb1406'>Индекс контроллер одключен</p>";
+	}
+}
