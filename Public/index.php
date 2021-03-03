86162bde95f6185ce3433e8c7641917886d9a148
Rinelsia <rinelsia@mail.ru>
2021-03-04 01:45:24 +0300

    Add files via upload

---
+++ Public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// запуск загрузчика
+// require_once __DIR__."\loader\loader.php";
+require_once __DIR__.'/../sys/start.php';
+// запуск файла для работы с фар архивом. Это нужно для того чтобы извлечь данные из композера. 
+// может быть верменным решением
+// require_once __DIR__.'/phar.php';
+// закуск композера
+include __DIR__.'/../Vendor/autoload.php';
+
+
